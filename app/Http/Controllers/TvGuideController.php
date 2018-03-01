eeadde691d8527e837341cf6199b6440e75b5a48
Rafarlox <rafarlox@gmail.com>
2018-03-01 15:14:13 +0100

    first commit

---
+++ app/Http/Controllers/TvGuideController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TvGuideController extends Controller
+{
+
+    public function InitCurlTvShowsGuide($station, $date, $bu) {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => 'https://api.srgssr.ch/epg/v2/tvshows/stations/rts-1?date=2018-03-01&bu=SRF',
+            CURLOPT_HTTPHEADER => array('accept: application/json', 'Authorization:  ', 'Authorization: Bearer H7WjusPyormcKPVGPU06DgC0VgdV')
+        ));
+
+        $result = curl_exec($curl);
+
+        return $result;
+    }
+
+
+    public function GetGuideTv($station, $date, $bu)
+    {
+        $curl = $this->InitCurlTvShowsGuide($station, $date, $bu);
+
+        $datas = json_decode($curl);
+
+        $tvGuide = array();
+
+        foreach($datas as $data)
+        {
+            $epg = array(
+                $data->title,
+                $data->shortDescription,
+                $data->actors,
+            );
+
+            array_push($titles, $epg);
+        }
+        dd($tvGuide);
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+API of the project WatchMe
