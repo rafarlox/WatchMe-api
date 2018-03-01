6014dab794e14d267a9dcf2c41083d02c7f68102
jdkuenzi <jeandanielkuenzi@gmail.com>
2018-03-01 18:11:56 +0100

    Conflict

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
-Route::get('/title/station/{station}/date/{date}/bu/{bu}', 'TvGuideController@GetGuideTv');
+Route::get('/title/station/{station}/date/{date}/bu/{bu}', 'TvGuideController@GetDataFromEPG');
 
 Route::get('/tmdb/title/{title}', 'TvGuideController@GetInfoByTMDb');
-=======
-Route::get('/title/station/{station}/date/{date}/bu/{bu}', 'TvGuideController@GetDataFromEPG');
->>>>>>> 59eaace5958cae1e922b812129e074af61d73193
